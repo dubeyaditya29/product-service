e7891f4530229db20ddf7ab6100bccd7bf7419c2
aditya dubey <48158220+dubeyaditya29@users.noreply.github.com>
2024-01-03 19:46:21 +0530

    adding testContainer for mongodb

--- src/test/java/com/shoppingApplication/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/shoppingApplication/productservice/ProductServiceApplicationTests.java
@@ -2,10 +2,16 @@ package com.shoppingApplication.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
 
+	@Container
+	MongoDBContainer mongoDBContainer = new MongoDBContainer();
 	@Test
 	void contextLoads() {
 	}
